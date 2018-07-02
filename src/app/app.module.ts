fd0265364ff5fa125756f5c2b3e3aaaa9f1ceba7
Maykon Oliveira <maykonoliveira850@gmail.com>
2018-07-02 20:55:49 -0300

    implementação login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from '../routing/app-routing.module';
+import { AppRoutingModule } from './routing/app-routing.module';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireStorageModule } from 'angularfire2/storage';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { environment } from '../environments/environment';
 import { LoginComponent } from './login/login.component';
 import { TelaPrincipalComponent } from './tela-principal/tela-principal.component';
-import {FormsModule} from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { ToastrModule, ToastNoAnimation, ToastNoAnimationModule, } from 'ngx-toastr';
 
 @NgModule({
   declarations: [
@@ -18,7 +24,19 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
+    AngularFireStorageModule,
+    ToastNoAnimationModule,
+    ToastrModule.forRoot({
+      progressBar: true,
+      timeOut: 5000,
+      progressAnimation: 'increasing',
+      maxOpened: 3,
+      toastComponent: ToastNoAnimation,
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
