2101e9e9bed199606f9aa40eb7bfcc11fee10144
Maykon Oliveira <maykonoliveira850@gmail.com>
2018-07-04 13:39:47 -0300

    add dependencia MQTT

--- src/app/tela-principal/tela-principal.component.ts
+++ src/app/tela-principal/tela-principal.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { ToastrService } from 'ngx-toastr';
+import * as mqtt from 'mqtt';
 
 @Component({
   selector: 'app-tela-principal',
@@ -8,20 +9,22 @@ import { ToastrService } from 'ngx-toastr';
   styleUrls: ['./tela-principal.component.css']
 })
 export class TelaPrincipalComponent implements OnInit {
-
+  clienteMQTT: mqtt.MqttClient;
   turnON = '../../assets/imgs/turnON.png';
   turnOFF = '../../assets/imgs/turnOFF.png';
   url_api = 'http://api.thingspeak.com/channels/529312/feed/last.json';
 
-  constructor(private http: HttpClient, private toastr: ToastrService) { }
-  
+  constructor(private http: HttpClient, private toastr: ToastrService) {
+    this.clienteMQTT = mqtt.connect('mqtt://mqtt.thingspeak.com:1883', { clientId: 'cliente2' });
+  }
+
   ngOnInit() {
     this.toastr.success('Bem Vindo!');
-    this.http.get(this.url_api).subscribe (
+    this.http.get(this.url_api).subscribe(
       dados => {
         console.log(dados);
         return dados;
-    });
+      });
   }
 
 }
